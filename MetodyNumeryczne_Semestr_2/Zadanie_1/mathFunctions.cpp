b4ae1c2f21d36b7997d5ac38fb39d3fe9f0b73e6
lukassz111 <lukassz111@gmail.com>
2019-03-09 21:03:14 +0100

    funkcja trygonometryczna

--- MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctions.cpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctions.cpp
@@ -53,6 +53,7 @@ namespace mathFunction
 		arr.push_back(createFunctionMeta(mathFunctionList::same, "x"));
 		arr.push_back(createFunctionMeta(mathFunctionList::Polynomial_1, "4x^3+2x^2+x-5"));
 		arr.push_back(createFunctionMeta(mathFunctionList::Polynomial_2, "x^2+x-3"));
+		arr.push_back(createFunctionMeta(mathFunctionList::Trygonometric_1, "4sin(2x)"));
 		return arr;
 	}
 }
--- MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctionsList.hpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctionsList.hpp
@@ -7,4 +7,6 @@ namespace mathFunctionList
 	double Polynomial_1(double x);// 4x^3+2x^2+x-5
 
 	double Polynomial_2(double x);// x^2+x-3
+
+	double Trygonometric_1(double x);// 4sin(2x)
 }
