a2516e3fa20a79d7f623f0fa1e1af1eb93428ec9
kamilakossyn <kamila.kossyn@gmail.com>
2019-03-09 21:02:23 +0100

    metody w main

--- MetodyNumeryczne_Semestr_2/Zadanie_1/main.cpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/main.cpp
@@ -5,7 +5,6 @@
 
 using namespace std;
 
-
 int main(int argc, char* argv[]) {
 	double x = 3;
 	mathFunction::functionMeta f, g;
@@ -16,6 +15,17 @@ int main(int argc, char* argv[]) {
 	cout << "f(x0)=" << f.func(x) << endl;
 	cout << "g(x0)=" << g.func(x) << endl;
 	cout << "f(g(x0))=" << mathFunction::superposition(f.func, g.func, x) << endl;
+	double intervalStart;
+	double intervalEnd;
+	cout << "Start point for search interval:";
+	cin >> intervalStart;
+	cout << "End point for search interval:";
+	cin >> intervalEnd;
+	double firstX;
+	cin >> firstX; //need for newton method
+	double bisectionX = bisection( intervalStart, intervalEnd, f.func );
+	double newtonX = newtonMetod( intervalStart, intervalEnd, firstX, f.func );
+	cout << "Bisection method =" << bisectionX << '\n' << "Newton metod =" << newtonX << '\n';
 	cin.get();
 	cin.get();
 	cin.get();
