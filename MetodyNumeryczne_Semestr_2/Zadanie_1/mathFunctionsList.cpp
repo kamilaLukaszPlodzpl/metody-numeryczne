b764c9fb6a4a0ca29376a60e7c8557785230ca48
lukassz111 <lukassz111@gmail.com>
2019-03-10 21:34:59 +0100

    pochodne

--- MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctionsList.cpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/mathFunctionsList.cpp
@@ -13,27 +13,27 @@ namespace mathFunctionList
 		return 4 * (x*x*x) + 2 * (x*x) + x - 5;
 	}
 	double derivative_Polynomial_1(double x) {
-		return 1;
+		return 12*(x*x) + 4*x + 1;
 	}
 
 	double Polynomial_2(double x) {// x^2+x-3
 		return x*x + x - 3;
 	}
 	double derivative_Polynomial_2(double x) {
-		return 1;
+		return 2*x+1;
 	}
 
 	double Trygonometric_1(double x) {// 4sin(2x)
 		return 4 * sin(2 * x);
 	}
 	double derivative_Trygonometric_1(double x) {
-		return 1;
+		return 8 * cos(2 * x);
 	}
 
-	double Exponetial_1(double x) {// 2^x
-		return exp2(x);
+	double Exponetial_1(double x) {// e^x
+		return exp(x);
 	}
 	double derivative_Exponetial_1(double x) {
-		return 1;// TODO
+		return exp(x);
 	}
 }
