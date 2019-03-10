611c6c9051506ca89270f74790ff949fbd366bb9
lukassz111 <lukassz111@gmail.com>
2019-03-10 19:53:16 +0100

    Poprawka jednego if'a w kodzie kamilii Moja wersja tego samego w komentarzu.

--- MetodyNumeryczne_Semestr_2/Zadanie_1/bisectionMetod.cpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/bisectionMetod.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//* Kamila
 double bisectionA(mathFunction::function func, double a, double b, double E, int &iteration) {
 	if ((func(a)) * (func(b)) > 0) {
 		cout << "Function doesn't meet essential assumptions"; // wartosc funkcji na krancach przedzialu musi miev rozne znaki
@@ -19,12 +20,41 @@ double bisectionA(mathFunction::function func, double a, double b, double E, int
 		if (func(x)*(func(a)) < 0) {
 			b = x;
 		}
-		a = x;
+		else
+		{
+			a = x;
+		}
 		//cout << x << iteration;
 	}
 	return x;
 }
+//*/
 
+/* £ukasz
+double bisectionA(mathFunction::function func, double a, double b, double epsilon, int &iteration) {
+	if (func(a)*func(b) >= 0) {
+		cout << "Error, f(" << a << ") * f(" << b << ") >= 0. f(a)*f(b) must be less than zero!" << endl;
+		iteration = -1;
+		return NULL;
+	}
+	iteration = 1;
+	while (abs(a-b) > epsilon) {
+		double x1 = (a + b) / 2;
+		if (abs(func(x1)) < epsilon)
+			return x1;
+
+		if ((func(a) * func(x1)) < 0) {
+			b = x1;
+		}
+		else {
+			a = x1;
+		}
+		iteration++;
+	}
+	double x1 = (a + b) / 2;
+	return x1;
+}
+//*/
 double bisectionB(mathFunction::function func, double a, double b, double E, int &iteration) {
 	if ((func(a)) * (func(b)) > 0) {
 		cout << "Function doesn't meet essential assumptions";
@@ -43,3 +73,4 @@ double bisectionB(mathFunction::function func, double a, double b, double E, int
 	//E = abs( 0 - func(x) ); dok³adnoœæ przybli¿enia do 0
 	return x;
 }
+
