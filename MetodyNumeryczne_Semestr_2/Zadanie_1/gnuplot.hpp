9dc23d4172a45d045c2caa683a0a6e99dc41138a
lukassz111 <lukassz111@gmail.com>
2019-03-12 15:39:14 +0100

    cccc

--- MetodyNumeryczne_Semestr_2/Zadanie_1/gnuplot.hpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/gnuplot.hpp
@@ -1,6 +1,7 @@
 #pragma once
+#include "mathFunctions.hpp"
 
 namespace gnuplot
 {
-
+	void generateScript(mathFunction::function f, const double &a, const double &b, const double &resolution, const char *fileName);
 }
