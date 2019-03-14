b992d57e417d8837aa58ed3a002cd3c3e5401a92
lukassz111 <lukassz111@gmail.com>
2019-03-14 22:58:40 +0100

    raport_generator

--- MetodyNumeryczne_Semestr_2/Zadanie_1/main.cpp
+++ MetodyNumeryczne_Semestr_2/Zadanie_1/main.cpp
@@ -4,10 +4,28 @@
 #include "bisectionMetod.hpp"
 #include "newtonMethod.hpp"
 #include "gnuplot.hpp"
-
-
+#include <fstream>
 
 using namespace std;
+
+void makeRaport(const char *raportFileName, mathFunction::functionMeta f, double a, double b,const char *method,
+	double bisectionX, double bisectionIteration, double bisectionEpsilon,
+	double newtonX, double newtonIteration, double newtonEpsilon)
+{
+	fstream raport(raportFileName, ios::out);
+	raport << "Function: f(x)=" << f.name << "\n";
+	raport << "range - ( " << a << ", " << b << " )\n";
+	raport << method << "\n";
+	raport << "Bisection: \n";
+	raport << "\t x=" << bisectionX << "\n";
+	raport << "\t epsilon=" << bisectionEpsilon << "\n";
+	raport << "\t iteration=" << bisectionIteration << "\n";
+	raport << "Newton: \n";
+	raport << "\t x=" << newtonX << "\n";
+	raport << "\t epsilon=" << newtonEpsilon << "\n";
+	raport << "\t iteration=" << newtonIteration << "\n";
+	raport.close();
+}
 
 void algorithmAccurancy(mathFunction::functionMeta f,const double &intervalStart, const double &intervalEnd) {
 	double epsilon;
@@ -19,11 +37,13 @@ void algorithmAccurancy(mathFunction::functionMeta f,const double &intervalStart
 	double bisectionX = bisectionA(f.func, intervalStart, intervalEnd, epsilon, bisectionIteration);
 	double newtonX = newtonA(f.func, f.derivativeFunc, intervalStart, intervalEnd, epsilon, newtonIteration);
 
-	cout << "Bisection method =" << scientific << bisectionX << "    Iteration = " << bisectionIteration << '\n';
-	cout << "Newton metod =" << scientific << newtonX << "     Iteration = " << newtonIteration << '\n';
+	cout << "Bisection method =" << bisectionX << "    Iteration = " << bisectionIteration << '\n';
+	cout << "Newton metod =" << newtonX << "     Iteration = " << newtonIteration << '\n';
 
 	gnuplot::displayFunction(0.001, f, intervalStart, intervalEnd, true, bisectionX, "bisectionX", newtonX, "newtonX");
-
+	string raportName = f.name;
+	raportName += ".txt";
+	makeRaport(raportName.c_str(), f, intervalStart, intervalEnd, "Accurancy", bisectionX, bisectionIteration, epsilon, newtonX, newtonIteration, epsilon);
 }
 
 void algorithIteration(mathFunction::functionMeta f, const double &intervalStart, const double &intervalEnd) {
@@ -41,6 +61,9 @@ void algorithIteration(mathFunction::functionMeta f, const double &intervalStart
 	cout << "Newton metod =" << scientific << newtonX << "Acurrancy = " << scientific << newtonEpsilon << '\n'; // wyswietlanie poprawic 
 
 	gnuplot::displayFunction(0.001, f, intervalStart, intervalEnd, true, bisectionX, "bisectionX", newtonX, "newtonX");
+	string raportName = f.name;
+	raportName += ".txt";
+	makeRaport(raportName.c_str(), f, intervalStart, intervalEnd, "Accurancy", bisectionX, iteration, bisectionEpsilon, newtonX, iteration, newtonEpsilon);
 
 }
 
